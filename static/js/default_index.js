21a2f99cf39ae9c928f37fdfdbd094269d998836
Sean Odnert <sosharkie@gmail.com>
2018-06-06 11:24:14 -0700

    Price now updates dynamically

--- static/js/default_index.js
+++ static/js/default_index.js
@@ -23,11 +23,12 @@ var app = function() {
                 self.vue.boards = data.boards;
                 console.log(self.vue.boards);
             });
-    }
+    };
 
     self.change_page = function(new_page) {
         self.vue.page = new_page;
-    }
+        self.update_board_options();
+    };
 
     self.add_board = function () {
         $.post(add_board_url,
@@ -50,6 +51,46 @@ var app = function() {
                 //console.log(self.vue.boards);
             }
         );
+    };
+
+    self.update_board_options = function() {
+        var price = 0;
+        //Board type
+        if (self.vue.board_type == 'Shortboard'){
+            price += 200;
+        } else if (self.vue.board_type == 'Hybrid'){
+            price += 250;
+        } else if (self.vue.board_type == 'Longboard'){
+            price += 300;
+        }
+        //Tail type
+        if (self.vue.board_tail_type == 'Square'){
+            price += 25;
+        } else if (self.vue.board_tail_type == 'Pin'){
+            price += 30;
+        } else if (self.vue.board_tail_type == 'Swallow'){
+            price += 35;
+        } else if (self.vue.board_tail_type == 'Diamond'){
+            price += 40;
+        }
+        //color
+        if (self.vue.board_color == 'White' || self.vue.board_color == 'Grey'){
+            price += 20;
+        } else if (self.vue.board_color == 'Blue' || self.vue.board_color == 'Green' || 
+            self.vue.board_color == 'Red' || self.vue.board_color == 'Orange'){
+            price += 30;
+        } else if (self.vue.board_color == 'Black' || self.vue.board_color == 'Purple' ||
+            self.vue.board_color == 'Yellow'){
+            price += 40;
+        } 
+        //fins
+        price += (self.vue.num_of_fins * 20);
+        //length
+        price += (self.vue.board_length * 10);
+        //volume
+        price += (self.vue.board_volume * 2);
+
+        self.vue.board_price = price;
     }
 
     self.set_price_filter = function(min, max){
@@ -58,13 +99,13 @@ var app = function() {
         //console.log(self.vue.min_price);
         //console.log(self.vue.max_price);
         self.get_boards();
-    }
+    };
 
     self.set_board_type_filter = function(board_type){
         self.vue.board_type_filter = board_type;
         console.log(self.vue.board_type_filter);
         self.get_boards();
-    }
+    };
 
     self.vue = new Vue({
         el: "#vue-div",
@@ -76,7 +117,7 @@ var app = function() {
             boards: [],
 
             // Customization options
-            board_price: 500,
+            board_price: 0,
             board_type: 'Shortboard',
             board_tail_type: 'Square',
             num_of_fins: 3,
@@ -93,6 +134,7 @@ var app = function() {
             get_boards: self.get_boards,
             change_page: self.change_page,
             add_board: self.add_board,
+            update_board_options: self.update_board_options,
             set_price_filter: self.set_price_filter,
             set_board_type_filter: self.set_board_type_filter,
         }
